817e2b775e5ddb768381bd63a1c5c9b3037a283a
ClaytonForbes <101clay@live.com>
2024-03-07 16:10:32 -0500

    Change Skills section a little

--- my-app/src/Pages/Skills.jsx
+++ my-app/src/Pages/Skills.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { TypeAnimation } from 'react-type-animation';
 import data from "../data"
 
 const MySkills = () =>{
@@ -12,7 +13,21 @@ return (
   <section className="skills--section" id="mySkills">
     <div className="portfolio--container">
       <p className="section--title">My Skills</p>
-      <h2 className="skills--section--heading">I have Experience:</h2>
+      <h2 className="skills--section--heading"> <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'I am a Software Developer Currently',
+                3000, // wait 1s before replacing "Mice" with "Hamsters"
+                'I am a Full Stack Developer',
+                3000,
+                ' I am also an IT Technicain and Speaialist',
+                3000,
+              ]}
+              wrapper="span"
+              speed={50}
+              style={{ fontSize: '1em', display: 'inline-block' }}
+              repeat={Infinity}
+              /></h2>
     </div>
     {/* <div className={`skills--section--container${spread ? '--expanded-container' : ''}`} onClick={toggleSpread}>
       <div className="skills--section--card">
